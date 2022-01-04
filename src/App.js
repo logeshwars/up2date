a1d21b7b01efb7b06583f5e05fa8831b8e52af17
logeshwars <stylishstarlogesh15@gmail.com>
2022-01-04 15:02:26 +0530

    admin sidebar added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Main from './components/Main'
 import NavBar from './components/NavBar';
 import Admin from './components/Admin';
+import AdminNav from './components/AdminNav';
 function App() {
   return (
     <div className="app">
       <Router>
-        <NavBar/>
         <Routes>
-          <Route path='/'  element={<Main/>}/>
-          <Route path='admin'  element={<Admin/>}/>
+          <Route path='/'  element={<><NavBar/><Main/></>}/>
+          <Route path='admin'  element={<><AdminNav/><Admin/></>}/>
         </Routes>
       </Router>
     </div>
---
+++ src/components/AdminNav.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { auth} from "../firebase";
+import { Navigate } from 'react-router-dom';
+function AdminNav() {
+    const[gotoMain,setgotoMain]=useState(false);
+    const signOut=()=>{
+        auth
+        .signOut()
+        .then(() => {
+         setgotoMain(true);
+        })
+        .catch((error) => console.log(error.message));
+    };
+    
+    return (
+        <div className="navBar">
+             {gotoMain&&(<Navigate to="/" replace />)}
+        <div>
+          <span className="logo">
+            up<span className="logoMiddle">2</span>date
+          </span>{" "}
+          <span className="logoEvent">events</span>
+        </div>
+        <h3>Admin</h3>
+        <div className="loginButtonHolder">
+          <button className="loginButton" onClick={()=>signOut()}>Sign out</button>
+        </div>
+        </div>
+    )
+}
+
+export default AdminNav
