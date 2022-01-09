4c6a5ade11831b7bb146d5105584cde0ffeac3a2
logeshwars <stylishstarlogesh15@gmail.com>
2022-01-09 12:44:53 +0530

    ui modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Main from './components/Main'
+import Hero from './components/Hero'
 import NavBar from './components/NavBar';
 import Admin from './components/Admin';
 import AdminNav from './components/AdminNav';
@@ -9,6 +10,7 @@ import { db} from "./firebase";
 import { collection, getDocs } from "firebase/firestore";
 import date from './util/currentDate'
 import { doc, onSnapshot } from "firebase/firestore";
+import EventDetails from "./components/EventDetails";
 function App() {
   const [events,setEvents]=useState([]);
   const [user,setUser]=useState([]);
@@ -41,7 +43,8 @@ function App() {
     <div className="app">
       <Router>
         <Routes>
-          <Route path='/'  element={<><NavBar/><Main events={events}/></>}/>
+          <Route path='/'  element={<><NavBar/><Hero/> <Main events={events}/></>}/>
+          <Route path='details'  element={<><NavBar/><EventDetails/></>}/>
           <Route path='admin'  element={<><AdminNav/><Admin/></>}/>
         </Routes>
       </Router>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,6 +14,7 @@ function NavBar() {
   }, [showLogin])
   return (
     <div className="navBar">
+      <div className="navBarInside">
       <div>
         <span className="logo">
           up<span className="logoMiddle">2</span>date
@@ -37,6 +38,22 @@ function NavBar() {
       </div>
       <div className="loginButtonHolder">
         <button className="loginButton" onClick={()=>showlogin()}>Sign In</button>
+      </div>
+      </div>
+      <div className="searchBox searchBoxMin">
+        <input type="text" placeholder="search events here"/>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            fill="currentColor"
+            class="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </button>
       </div>
       {showLogin&&( <Login closeLogin={showlogin}/>)} 
     </div>
